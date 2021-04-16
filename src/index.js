7f7e963bd484ac0b8926845b7b890336294e5472
enric <enric.velasco.g@gmail.com>
2021-04-16 22:42:05 +0200

    receive first portal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-const CustomComponent = () => (
-  <h1>
-    Easy portals v1
-  </h1>
+let clientPortalsList = null
+
+export const createPortal = ({ portalId, ...props }) => {
+  const component = <PortalElement portalId={portalId} {...props} />
+  const portalContainer = document.createElement('div')
+  portalContainer.setAttribute('id', portalId)
+  document.body.appendChild(portalContainer)
+  ReactDOM.render(component, document.getElementById(portalId))
+}
+
+export const hidePortalById = portalId => {
+  document.getElementById(portalId).remove()
+}
+
+const RenderPortal = ({ portalId, Component, ...props }) => (
+  <Component
+    id={portalId}
+    close={() => hidePortalById(portalId)}
+    {...props}
+  />
 )
 
-export default CustomComponent
+export const PortalElement = ({ portalId, ...props }) => (
+  ReactDOM.createPortal(
+    <RenderPortal
+      portalId={portalId}
+      Component={clientPortalsList[portalId]}
+      {...props}
+    />,
+    document.getElementById(portalId)
+  )
+)
+
+const Portals = ({ portalsList }) => {
+  clientPortalsList = portalsList
+
+  return (<></>)
+}
+
+export default Portals
